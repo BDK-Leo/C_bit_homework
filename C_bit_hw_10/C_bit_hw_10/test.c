047d5ceeeab39eaa9b7cf0e8cf2542f7e699a6a5
bdkleo <mizuo6188@gmail.com>
2025-07-06 16:49:21 +0800

    指针（二）

--- C_bit_hw_10/C_bit_hw_10/test.c
+++ C_bit_hw_10/C_bit_hw_10/test.c
@@ -17,3 +17,38 @@
 //	}
 //	return 0;
 //}
+
+
+//题目二：冒泡排序
+//实现一个对整形数组的冒泡排序
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = {1,3,5,7,9,2,4,6,8,10};
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	int flag = 1;
+//	for (i = 0;i < sz - 1;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < sz - 1 - i;j++)
+//		{
+//			if (arr[j] > arr[j+1])
+//			{
+//				flag = 0;
+//				int tmp = arr[j + 1];
+//				arr[j + 1] = arr[j];
+//				arr[j] = tmp;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//	for (i = 0;i < sz;i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//	return 0;
+//}
