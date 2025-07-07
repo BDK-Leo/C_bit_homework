beb11b734d8716cfcb5006f24cb0f8995eaf0999
bdkleo <mizuo6188@gmail.com>
2025-07-07 16:59:39 +0800

    指针（二）

--- C_bit_hw_10/C_bit_hw_10/test.c
+++ C_bit_hw_10/C_bit_hw_10/test.c
@@ -188,3 +188,38 @@
 //	FindKiller();
 //	return 0;
 //}
+
+
+//题目六：杨辉三角，在屏幕上打印杨辉三角。
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int arr[30][30] = {0};
+	scanf("%d",&n);
+	int i = 0;
+	int j = 0;
+	for (i = 0;i < n;i++)
+	{
+		for (j = 0;j <= i;j++)
+		{
+			if (j == 0 || i == j)
+			{
+				arr[i][j] = 1;
+			}
+			if (i >= 2 && j >= 1)
+			{
+				arr[i][j] = arr[i - 1][j - 1] + arr[i-1][j];
+			}
+		}
+	}
+	for (i = 0;i < n;i++)
+	{
+		for (j = 0;j <= i;j++)
+		{
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
