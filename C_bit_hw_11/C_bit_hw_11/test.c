a210446991dd075b28a6b10e45f3bd53d8cefe69
bdkleo <mizuo6188@gmail.com>
2025-07-08 20:33:20 +0800

    指针（三）

--- C_bit_hw_11/C_bit_hw_11/test.c
+++ C_bit_hw_11/C_bit_hw_11/test.c
@@ -59,9 +59,21 @@
 #include<stdio.h>
 int main()
 {
-	while (~scanf())
+	int y = 0;
+	int m = 0;
+	int day = 0;
+	int arr[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	while (~scanf("%d%d",&y,&m))
 	{
-
+		day = arr[m];
+		if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+		{
+			if (m == 2)
+			{
+				day += 1;
+			}
+		}
+		printf("%d\n",day);
 	}
 	return 0;
 }
